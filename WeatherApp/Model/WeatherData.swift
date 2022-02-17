cdf5a35104483b9bc859a989643bf471238d302e
sumwb <wnadud761@gmail.com>
2022-02-17 20:57:34 +0900

    add 2 Files

---
+++ WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by sumwb on 2022/02/17.
+//
+
+import Foundation
---
+++ WeatherApp/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by sumwb on 2022/02/17.
+//
+
+import Foundation
