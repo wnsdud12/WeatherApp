115c914154417cef7700293fc09593d90be3fa1f
wnsdud12 <wnadud761@gmail.com>
2022-03-22 17:15:58 +0900

    WeatherModel 수정 완료

--- WeatherApp/Controller/ViewController.swift
+++ WeatherApp/Controller/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- WeatherApp/Model/WeatherModel.swift
+++ WeatherApp/Model/WeatherModel.swift
@@ -12,52 +12,34 @@ typealias WeatherValue = [String: String]
 typealias WeatherTableData = (date: String, time: String, value: WeatherValue)
 
 struct WeatherModel {
-    var sections: [String]
-    var time: [[String]]
-    var value: [[WeatherValue]]
-    // tableView에 보여질 데이터
-    var cellTime: [String]
-    var cellTMP: [String]
-    var cellImgSKY: [UIImage]
-    var cellSKY: [String]
-    var cellPOP: [String]
-    var cellPCP: [String]
-
-    // 최저/최고 온도
-    var arrTMX: [String]
-    var arrTMN: [String]
-
+    var sections: [String] //init ok
+    var cellTime: [[String]] //init ok
+    var cellTMP: [[String]] //init ok
+    var cellSKY: [[String]] //init ok
+    var cellPOP: [[String]] //init ok
+    var cellPCP: [[String]] //init ok
     init(date:[String], time: [String], value: [WeatherValue]) {
-        print("weatherModel create")
-        print("date\n\(date)\n\n")
-        print("time\n\(time)\n\n")
-        print("value\n\(value)\n\n")
-        let tupleSKY = setSKY(value: value, time: time)
-
-        self.cellTime = time.map {
-            let intTime: Int = Int($0)! / 100
-            let strTime = String(intTime) + "시"
-
-            return strTime
-        }
-        self.cellTMP = value.map{ $0["TMP"]! }
-        self.cellImgSKY = tupleSKY.image.map{ UIImage(named: $0)! }
-        self.cellSKY = tupleSKY.label
-        self.cellPOP = value.map{ $0["POP"]! }
-        self.cellPCP = value.map{ $0["PCP"]! }
-
-        self.arrTMN = value.filter{$0["TMN"] != nil}.map{ $0["TMN"]!}
-        self.arrTMX = value.filter{$0["TMX"] != nil}.map{ $0["TMX"]!}
 
         // date array remove duplicate
         let set = Set(date)
         let sections = Array(set).sorted{ $0 < $1 }
         self.sections = sections
 
+        // time & value Array split
         let splitArrays: (time: [[String]], value: [[WeatherValue]]) = splitIndex(time: time, value: value)
-        self.time = splitArrays.time
-        self.value = splitArrays.value
+        let cellTime: [[String]] = splitArrays.time.compactMap {
+            $0.compactMap{
+                convertTimeString(fcstTime:$0)
+            }
+        }
+        self.cellTime = cellTime
 
+        let value: [[WeatherValue]] = splitArrays.value
+
+        self.cellTMP = value.compactMap { $0.compactMap {$0["TMP"]} }
+        self.cellSKY = value.compactMap { $0.compactMap {$0["SKY"]} }
+        self.cellPOP = value.compactMap { $0.compactMap {$0["POP"]} }
+        self.cellPCP = value.compactMap { $0.compactMap {$0["PCP"]} }
     } // init()
 
 } // WeatherModel
@@ -87,39 +69,27 @@ func splitIndex(time: [String], value: [WeatherValue]) -> ([[String]], [[Weather
     return (splitTime, splitValue)
 }
 
-func setSKY(value: [WeatherValue], time: [String]) -> (image: [String], label: [String]) {
-    var isDay: Bool
-    var newImg: [String] = []
-    var newLabel: [String] = []
-    for i in value.indices {
-        // 6시~20시의 데이터는 낮 / 나머지는 밤
-        isDay = (6...20).contains(Int(time[i])!) ? true : false
-        if value[i]["PTY"] == "없음" {
-            newLabel.append(value[i]["SKY"]!)
-            switch value[i]["SKY"] {
-                case "맑음":
-                    isDay ? newImg.append("sunny.png") : newImg.append("night.png")
-                case "구름많음":
-                    isDay ? newImg.append("cloud_sun.png") : newImg.append("cloud_night.png")
-                case "흐림":
-                    newImg.append("cloudy.png")
-                default:
-                    break
-            }
-        } else {
-            switch value[i]["PTY"] {
-                case "비":
-                    newImg.append("rain.png")
-                case "비/눈":
-                    newImg.append("rainyORsnowy.png")
-                case "눈":
-                    newImg.append("snow.png")
-                case "소나기":
-                    newImg.append("rain_shower.png")
-                default:
-                    break
-            }
-        }
+// fcstDate로 받아온 날짜 문자열의 형식인 yyyyMMdd를 M월 d일 (E)로 변환
+// ex) 20220309 -> 3월 9일 (수)
+private func convertDateString(fcstDate: String) -> String {
+    var dateString: String = ""
+    var date: Date = Date()
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "ko_KR")
+    formatter.dateFormat = "yyyyMMdd"
+    date = formatter.date(from: fcstDate)!
+
+    formatter.dateFormat = "M월 d일 (E)"
+    dateString = formatter.string(from: date)
+    return dateString
+}
+// fcstTime로 받아온 시간 문자열의 형식인 HHmm을 HH시로 반환
+// ex) 1600 -> 16시
+private func convertTimeString(fcstTime: String) -> String {
+    var timeString: String = ""
+    if var intTime = Int(fcstTime) {
+        intTime = intTime / 100
+        timeString = String(intTime)
     }
-    return (newImg, newLabel)
+    return timeString + "시"
 }
